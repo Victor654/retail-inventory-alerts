19a25a834cdc91890ae03f3c3e3cda8cdf6dbcaf
Victor Manuel <naranjohoyosvictormanuel@gamil.com>
2026-02-11 21:55:06 -0500

    Structure for frontend and initil auth

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+// No necesitas nada aquí todavía
+</script>
+
+<style>
+#app {
+  font-family: system-ui, Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
